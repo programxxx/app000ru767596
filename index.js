d8867c4c38806e86ce7ad09fdbbad6d2e48828e6
programxxx <43172306+programxxx@users.noreply.github.com>
2019-11-15 22:48:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 ;(() => {
 	'use strict';
+	const mongoose = require('mongoose');
+	mongoose.connect(process.env.DATABASE_URL, {
+		useNewURLParser: true
+	});
+	const db = mongoose.connection;
+	let dbError = false;
+	db.on('error', err => {
+		console.log(err);
+		dbError = err;
+		setTimeout(() => {
+			dbError = false;
+		}, 1000);
+	});
+	db.once('open', () => {
+		console.log('Connected to mongoose OK');
+	})
 	require('http').createServer((req, res) => {
 		if (req.url === '/favicon.ico') {res.end();return;}
 		res.setHeader('Content-type', 'text/plain');
-		res.write(Math.random().toString(32).slice(2));
+		res.write(`Heroku node.js test OK ${Math.random().toString(32).slice(2)} ${new Date().toString()}${'\r\n'}`);
+		if (!dbError) {
+			res.write(`Database connection OK`);
+		} else {
+			res.write(dbError.toString())
+		}
 		res.end();
 	}).listen(process.env.PORT || 3000, err => {
 		if (err) {
